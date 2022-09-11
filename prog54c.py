063989fe60c5d40441c3c3ffd7c6b770c02fc562
Harperc21 <113038674+Harperc21@users.noreply.github.com>
2022-09-10 20:40:42 -0500

    Add files via upload

---
+++ prog54c.py
@@ -0,0 +1,15 @@
+# comment - Math operators: + - * /
+radius = float(input("Please Enter Length: "))
+
+
+radius = (3.712)
+
+
+pi = (3.14159)
+
+circumfrence = 2 * pi * radius
+area = pi * (radius * radius)
+
+print("The radius is", radius)
+print("The area is", area)
+print("The circumfrence is", circumfrence)
