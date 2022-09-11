bd5f3f2ce949fa1ef97e877354a5ed17d8e71566
Cameron Harper <113038674+Harperc21@users.noreply.github.com>
2022-09-11 00:24:27 +0000

    Update

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+length = int(input("Please Enter Length: "))
+width = int(input("Please Enther Width: "))
+
+print("The Length is", length)
+print("The Length is", width)
+
+area = length * width
+perimeter = 2 * length + 2 * width
+
+print("The area is", area)
+print("The perimeter is", perimeter)
---
+++ prog54b.py
@@ -0,0 +1,11 @@
+# comment - Math operators: + - * /
+num1 = float(input("Please Enter Length: "))
+num2 = float(input("Please Enter Length: "))
+num3 = float(input("Please Enter Length: "))
+num4 = float(input("Please Enter Length: "))
+
+_sum = num1 + num2 + num3 + num4
+average = (num1 + num2 + num3 + num4) / 4 
+
+print("The sum is", _sum)
+print("The average is", average)
