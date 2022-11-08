5a3bd9171d59551f55706e93f1c0c7b8495bc089
Harperc21 <113038674+Harperc21@users.noreply.github.com>
2022-11-08 09:15:48 -0600

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+dctnry = {}
+dctnry2 = {1: "apples", 2: "oranges", 3: "pears"}
+dctnry3 = {"name": "John", "clr": "red", 0: [1, 2, 3]}
+
+print(dctnry2[2])
+print(dctnry3["name"])
+print(dctnry3[0])
+print(dctnry2.keys())
+print(dctnry2.values())
